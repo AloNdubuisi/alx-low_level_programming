1e929f94fea591fce8e62ab4c4b00f28135ed567
Alo Ndubuisi <alo.ndubuisi07@gmail.com>
2022-10-16 14:54:56 -0700

    made some adjustment

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i);
-		if (i != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		putchar((i % 10) + '0');
+		if (i == 0)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
-	}
 }
