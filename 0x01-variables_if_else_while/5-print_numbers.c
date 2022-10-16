74432aed3fa102852c850767e2ac81c22c257f9d
Alo Ndubuisi <alo.ndubuisi07@gmail.com>
2022-10-16 12:38:15 -0700

    5-print_numbers.c script

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers from 0 - 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		printf('"%d", number);
+
+	printf("\n");
+
+	return (0);
+}
