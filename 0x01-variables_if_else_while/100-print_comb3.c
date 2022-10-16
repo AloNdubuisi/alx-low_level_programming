c20872d9847b663f920e19407abdd728a3003e35
Alo Ndubuisi <alo.ndubuisi07@gmail.com>
2022-10-16 15:43:42 -0700

    adjusted some errors

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints all possible different combinations of 2 digits
- * Returns: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -16,7 +16,7 @@ int main(void)
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
 
-			if (digit1 == 8 && digit2 == 0)
+			if (digit1 == 8 && digit2 == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
