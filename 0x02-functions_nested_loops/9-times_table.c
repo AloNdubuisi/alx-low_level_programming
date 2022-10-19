2c225d16d5276b173988d52dc964ae98950d43b1
Alo Ndubuisi <alo.ndubuisi07@gmail.com>
2022-10-19 02:42:37 -0700

    made some corrections

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,14 +13,21 @@ void times_table(void)
 		_putchar(' ');
 		for (mult = 1; mult <= 9; mult++)
 		{
-			prod = num * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			else
+			prod = (num * mult);
+			if ((prod / 10) > 0)
 			{
 				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((prod % 10) + '0');
+
+			if (mult < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
