09c070c9308c1acf4d7a8c476f8732b6178dea60
Alo Ndubuisi <alo.ndubuisi07@gmail.com>
2022-10-19 05:14:57 -0700

    adjusted some errors

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * print_times_table - Entry point
+ * print_times_table - prints the times table of the input, starting with 0.
  * @n: input
  * Return: Always 0 (success)
  */
