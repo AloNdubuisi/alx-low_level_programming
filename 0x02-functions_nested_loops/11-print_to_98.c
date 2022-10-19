f00f1b27d9c7a1fd6eeece5301df2f8fd4bc62a9
Alo Ndubuisi <alo.ndubuisi07@gmail.com>
2022-10-19 03:45:39 -0700

    some adjustment made

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,36 +6,9 @@
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	for (n = 0; n <= 98; n++)
 	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n++;
-		}
+		printf("%d", n);
 	}
-	else if (n > 98)
-	{
-		while (n >= 98)
-		{
-			while (n >= 98)
-			{
-				printf("%d", n);
-				if (n != 98)
-				{
-					printf(", ");
-				}
-				n--;
-			}
-		}
-		else
-		{
-			printf("98");
-		}
-		printf("\n");
-	}
+	printf("\n");
 }
