8ffda2ad9c045eb83bfcc468c6896c83f9046bed
Alo Ndubuisi <alo.ndubuisi07@gmail.com>
2022-10-21 12:04:48 -0700

    some omissions corrected

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,16 @@ void more_numbers(void)
 {
 	int num, a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 10; a++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num > 9)
+			if (num >= 10)
 			{
 				_putchar((num / 10) + '0');
 			}
 			_putchar((num % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
