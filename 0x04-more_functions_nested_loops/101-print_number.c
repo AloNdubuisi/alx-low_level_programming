ee2cdd04f499b1cd9b5b07d2454064f3914c3e82
Alo Ndubuisi <alo.ndubuisi07@gmail.com>
2022-10-22 07:48:23 -0700

    made some changes on the logic

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,40 +7,16 @@
  */
 void print_number(int n)
 {
-	unsigned int k;
-	int m, b;
+	unsigned int k = n;
 
-	b = 10;
-
-	if (n < 10 && n >= 0)
+	if (n < 0)
 	{
-		_putchar(n + '0');
+		_putchar(45);
+		k = -k;
 	}
-	else if (n > -10 && n < 0)
+	if (k / 10)
 	{
-		n = n - 2 * n;
-		_putchar('-');
-		_putchar(n + '0');
+		print_number(k / 10);
 	}
-
-	else
-	{
-		if (n < 0)
-		{
-			n = n * -1;
-			_putchar('-');
-		}
-		k = n;
-		while (k / b > 9)
-		{
-			b = b * 10;
-		}
-		while (b > 0)
-		{
-			m = k / b;
-			k = k % b;
-			_putchar(n + '0');
-			b = b / 10;
-		}
-	}
+	_putchar(k % 10 + '0');
 }
