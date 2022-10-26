03ca9e149c5845e517889b6073bbce281de41739
Alo Ndubuisi <alo.ndubuisi07@gmail.com>
2022-10-26 16:15:00 -0700

    adjusted erors

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,7 +9,8 @@ char *cap_string(char *n)
 {
 	int i, x;
 	int cap = 32;
-	int separator[] = {',', ';', '.', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
+	int separator[] = {',', ';', '.', '?', '"';								'(', ')', '{', '}', ' ', '\n', '\t'};
+
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
