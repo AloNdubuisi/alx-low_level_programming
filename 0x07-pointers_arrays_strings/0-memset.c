6c91e0f2ab9118d40f4290dee54ebac15b775160
Alo Ndubuisi <alo.ndubuisi07@gmail.com>
2022-11-01 07:01:45 -0700

    updated

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,7 +9,7 @@
  *   description _memset: over there
  *   Return: A pointer to the filled memory area @s.
  */
-void *_memset(void *s, int b, size_t n)
+char *_memset(char *s, char b, unsigned int n);
 {
 	unsigned int index;
 	unsigned char *memory = s, value = b;
