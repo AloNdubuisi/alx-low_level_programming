0d9638bc983a10093b377744b68fa7bc30d2d71b
Alo Ndubuisi <alo.ndubuisi07@gmail.com>
2022-11-04 08:48:52 -0700

    updated

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,19 @@
  * @argv: Array name
  * Return: 0
  */
-int main(int argc, _attribute_((unused))char *argv)
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
+
 	return (0);
 }
